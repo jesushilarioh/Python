18c508fa486db1b31906bc6aaec571574d0a3530
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2020-08-02 23:30:34 -0500

    modified challenge #10

--- Starting Out with Python/Chapter 3/Programming Exercises/10.py
+++ Starting Out with Python/Chapter 3/Programming Exercises/10.py
@@ -10,10 +10,6 @@
 # should display a message indicating whether the amount 
 # entered was more than or less than one dollar.
 
-# INPUT
-# 1. prompt the user to 
-#   enter the number of pennies, nickels, dimes, and 
-#   quarters.
 PENNY = .01
 NICKEL = .05
 DIME = .10
@@ -29,34 +25,19 @@ nickels *= NICKEL # nickels = nickels * NICKEL
 dimes *= DIME # dimes = dimes * DIME
 quarters *= QUARTER # quarters = quarters * QUARTERS
 
-# print("pennies =", pennies)
-# print("nickels =", nickels)
-# print("dimes =", dimes)
-# print("quarters =", quarters)
-
 total = pennies + nickels + dimes + quarters
 
 if_won_or_lost = ""
 
-# PROCESS
 if total == 1.00:
     if_won_or_lost = "Won"
 
-# 2. If the total value of the coins entered 
-#   is equal to one dollar, the program should congratulate 
-#   the user for winning the game.
 else:
     if_won_or_lost = "Lost"
     if total > 1.00:
         print("The amount entered is greater than 1 dollar.")
     else:
         print("The amount entered is less than 1 dollar.")
-    # LOst
 
-# 3. Otherwise, the program 
-# should display a message indicating whether the amount 
-# entered was more than or less than one dollar.
-
-# OUTPUT
 print("You", if_won_or_lost)
 print("Your total was", total)
