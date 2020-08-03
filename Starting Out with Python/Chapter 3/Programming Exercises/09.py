2cdbd0f042ecca88ddd647826951b0d959bae644
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2020-08-02 23:03:28 -0500

    removed comments on challgne #9

--- Starting Out with Python/Chapter 3/Programming Exercises/09.py
+++ Starting Out with Python/Chapter 3/Programming Exercises/09.py
@@ -20,51 +20,38 @@
 # enters a number that is outside the range of 0 through 36.
 
 
-# INPUT
-# asks the user to enter a pocket number 
-# and displays whether the pocket is green, red, or black.
 user_pocket_number = int(input("Enter a pocket number: "))
 
-
-
-# if the user 
-# enters a number that is outside the range of 0 through 36.
 if user_pocket_number >= 0 and user_pocket_number <= 36:
-    # PROCESS
     user_pocket_color = ""
-    # • Pocket 0 is green.
+
     if user_pocket_number == 0:
         user_pocket_color = "green"
-    # • For pockets 1 through 10, the odd-numbered 
-    #   pockets are red and the even-numbered pockets are black.
+
     elif user_pocket_number >= 1 and user_pocket_number <= 10:
         if user_pocket_number % 2 == 0:
             user_pocket_color = "black"
         else:
             user_pocket_color = "red"
-    # • For pockets 11 through 18, the odd-numbered pockets 
-    #   are black and the even-numbered pockets are red.
+
     elif user_pocket_number >= 11 and user_pocket_number <= 18:
         if user_pocket_number % 2 == 0:
             user_pocket_color = "red"
         else:
             user_pocket_color = "black"
-    # • For pockets 19 through 28, the odd-numbered pockets 
-    #   are red and the even-numbered pockets are black.
+
     elif user_pocket_number >= 19 and user_pocket_number <= 28:
         if user_pocket_number % 2 == 0:
             user_pocket_color = "black"
         else:
             user_pocket_color = "red"
-    # • For pockets 29 through 36, the odd-numbered pockets 
-    #   are black and the even-numbered pockets are red.
+
     elif user_pocket_number >= 29 and user_pocket_number <= 36:
         if user_pocket_number % 2 == 0:
             user_pocket_color = "red"
         else:
             user_pocket_color = "black"
-            
-    # OUTPUT
+
     print("User pocket color = ", user_pocket_color)
 else:
     print("Error. Number must be between 0 and 36.\nRerun program and try again.")
