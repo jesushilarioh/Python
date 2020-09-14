758f701c88dde6eabe928a10121c122f7a21c611
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2020-09-14 17:07:47 -0500

    added and modified.

--- Starting Out with Python/Chapter 3/Programming Exercises/13.py
+++ Starting Out with Python/Chapter 3/Programming Exercises/13.py
@@ -13,7 +13,23 @@
 # Write a program that asks the user to enter the weight of a 
 # package then displays the shipping charges.
 
-# 1. asks the user to enter the weight of a package
-package_weight = float(input("\nEnter the package weight: "))
+weight_of_package = float(input("Enter weight of package: "))
+message = ""
+shipping_charges = 0.0
 
-# 2. displays the shipping charges.
+# 2. process info
+message = "Shipping charges = "
+
+if weight_of_package <= 2:
+    shipping_charges = weight_of_package * 1.50
+
+elif weight_of_package > 2 and weight_of_package <= 6:
+    shipping_charges = weight_of_package * 3.00
+
+elif weight_of_package > 6 and weight_of_package <= 10:
+    shipping_charges = weight_of_package * 4.00
+
+elif weight_of_package > 10:
+    shipping_charges = weight_of_package * 4.75
+
+print(message + "$" + format(shipping_charges, ',.2f'))
---
+++ Starting Out with Python/Chapter 3/Programming Exercises/15.py
@@ -0,0 +1,17 @@
+# 15. Time Calculator
+# Write a program that asks the user to enter a number of 
+# seconds and works as follows:
+# • There are 60 seconds in a minute. If the number of 
+# seconds entered by the user is greater than or equal to 
+# 60, the program should convert the number of seconds to 
+# minutes and seconds.
+# • There are 3,600 seconds in an hour. If the number of 
+# seconds entered by the user is greater than or equal to 3,600, 
+# the program should convert the number of seconds to hours, 
+# minutes, and seconds.
+# • There are 86,400 seconds in a day. If the number of 
+# seconds entered by the user is greater than or equal to 
+# 86,400, the program should convert the number of seconds to 
+# days, hours, minutes, and seconds.
+
+# 1. asks the user to enter a number of seconds
