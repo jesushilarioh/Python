6875dac9634475143ead40833f7801324b8235da
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2020-10-17 00:03:39 -0500

    addedg

--- Starting Out with Python/Chapter 3/Programming Exercises/15.py
+++ Starting Out with Python/Chapter 3/Programming Exercises/15.py
@@ -14,4 +14,42 @@
 # 86,400, the program should convert the number of seconds to 
 # days, hours, minutes, and seconds.
 
-# 1. asks the user to enter a number of seconds
+number_of_seconds = int(input("Enter number of seconds: "))
+message = ""
+
+if number_of_seconds < 0:
+    message = "Enter a number above 0.\n" + \
+              "Rerun program and try again."
+else:
+    if number_of_seconds >= 0 and number_of_seconds < 60:
+        seconds = format(number_of_seconds % 60)
+        message = "Seconds = " + seconds
+
+    elif number_of_seconds >= 60 and number_of_seconds < 3600:
+        minutes = format(number_of_seconds // 60)
+        seconds = format(number_of_seconds % 60)
+        message = "Minutes = " + minutes + \
+                " Seconds = " + seconds
+
+    elif number_of_seconds >= 3600 and number_of_seconds < 86400:
+        hours = format(number_of_seconds // 3600)
+        minutes = format((number_of_seconds % 3600) // 60)
+        seconds = format((number_of_seconds % 3600) % 60)
+        message = "Hours = " + hours + " " + \
+                "Minutes = " + minutes + " " + \
+                "Seconds = " + seconds + " "
+
+        print("Hours", hours, "Mintues = ", minutes, "Seconds = ", seconds, sep=" ")
+
+    elif number_of_seconds >= 86400:
+        days = format(number_of_seconds // 86400)
+        hours = format((number_of_seconds % 86400) // 3600)
+        minutes = format(((number_of_seconds % 86400) % 3600) // 60)
+        seconds = format(((number_of_seconds % 86400) % 3600) % 60)
+        message = "Days = " + days + " " + \
+                "Hours = " + hours + " " + \
+                "Minutes = " + minutes + " " + \
+                "Seconds = " + seconds + " "
+
+
+print (message)
---
+++ Starting Out with Python/Chapter 3/Programming Exercises/17.py
@@ -0,0 +1,43 @@
+# 17. Wi-Fi Diagnostic Tree
+# Figure 3-19 shows a simplified flowchart for 
+# troubleshooting a bad Wi-Fi connection. 
+# Use the flowchart to create a program that 
+# leads a person through the steps of fixing a 
+# bad Wi-Fi connection.
+
+print("Reboot the computer and try to connect.\n(Y/N) Enter Y for yes or N for no.")
+
+user_answer = input("Did that fix the problem? ")
+
+if user_answer == "Y" or user_answer == "y" or user_answer == "n" or user_answer == "N":
+
+    if user_answer == "n" or user_answer == "N":
+        print("Reboot the router and try to connect.")
+        user_answer = input("Did that fix the problem? ")
+
+        if user_answer == "Y" or user_answer == "y" or user_answer == "n" or user_answer == "N":
+
+            if user_answer == "n" or user_answer == "N":
+                print(" Make sure the cables \nbetween the router & modem\nare plugged in firmly.")
+                user_answer = input("Did that fix the problem? ")
+
+                if user_answer == "Y" or user_answer == "y" or user_answer == "n" or user_answer == "N":
+
+                    if user_answer == "n" or user_answer == "N":
+                        print("Move the router to\ntoa new location\nand try to connect")
+                        user_answer = input("Did that fix the problem? ")
+
+                        if user_answer == "Y" or user_answer == "y" or user_answer == "n" or user_answer == "N":
+                            print("Get a new router")
+
+                        else:
+                            print("Please enter \"Y\" for yes or \"N\" for no.")
+                else:
+                    print("Please enter \"Y\" for yes or \"N\" for no.")
+        
+        else:
+            print("Please enter \"Y\" for yes or \"N\" for no.")
+
+else:
+    print("Please enter \"Y\" for yes or \"N\" for no.")
+
