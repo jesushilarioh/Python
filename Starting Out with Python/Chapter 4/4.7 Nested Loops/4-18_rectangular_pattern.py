5015ea047365f668f372509b876972af89178344
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2022-04-28 19:38:48 -0500

    added

---
+++ Starting Out with Python/Chapter 4/4.7 Nested Loops/4-18_rectangular_pattern.py
@@ -0,0 +1,10 @@
+# This program displays a rectangluar pattern 
+# of asterisks. 
+rows = int(input('How many rows? ')) 
+cols = int(input('How many columns? '))
+
+for r in range(rows): 
+    for c in range(cols): 
+        print('*', end='')
+    print()
+
--- Starting Out with Python/Chapter 4/Checkpoint_4.20-4.24.py
+++ Starting Out with Python/Chapter 4/Checkpoint_4.20-4.24.py
@@ -1,20 +1,30 @@
 print()
 print('4.20')
 print('What does the phrase "garbage in, garbage out" mean? ')
+print("GIGO, refers to the fact that computers cannot tell the difference between good data and bad data.")
 
 print()
 print('4.21')
 print('Give a general description of the input validation process. ')
+print("When the input is given to a program, it should be inspected before it is processed.")
+print("If the input is invalid, the program should discard it and prompt the user to enter the correct data.")
 
 print()
 print('4.22')
 print('Describe the steps that are generally taken when an input validation loop is used to validate data. ')
+print('1. Get input, \
+       2. Is the input bad? \
+       3a. If bad, display message, get input again. \
+       3b. If good exit loop and contiue program.')
 
 print()
 print('4.23')
 print('What is a priming read? What is its purpose? ')
+print('1. Priming the read is the first input operation--just before the loop.  \
+       2. The purpose of a priming read is to get the first input value that will be tested by the validation loop.')
 
 print()
 print('4.24')
 print('If the input that is read by the priming read is valid, how many times will the input validation loop iterate? ')
-
+print('Never. In this case, the initial boolean expression in the while loop with prove true.\
+       Therefore, bypassing the loops iterative block and continuing the program.')
