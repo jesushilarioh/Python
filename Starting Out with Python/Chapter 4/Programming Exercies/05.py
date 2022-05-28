d92f43393e8c31b3e80f2374627e7835337b8a3c
jesushilarioh <jesushilariohernandez@gmail.com>
2022-05-27 23:05:59 -0500

    modified and updated program

--- Starting Out with Python/Chapter 4/Programming Exercies/05.py
+++ Starting Out with Python/Chapter 4/Programming Exercies/05.py
@@ -9,4 +9,26 @@
 #
 #
 
+NUMBER_OF_MONTHS = 12
 
+numberOfYears = int(input("Enter number of years: "))
+inchesOfRainfall = 0.0
+totalMonths = 0
+totalInchesInRainFall = 0
+
+for year in range(numberOfYears):
+    for month in range(NUMBER_OF_MONTHS):
+
+        inchesOfRainfall = float(input("How many inches of rainfall for month #" \
+                                       + format((month + 1)) + " in year " + "#" \
+                                       + format(year + 1) + ": "))
+        totalMonths += 1
+        totalInchesInRainFall += inchesOfRainfall
+
+averageRainfall = totalInchesInRainFall / totalMonths
+
+output = "\nNumber of months           = " + format(totalMonths) + "\n"              \
+         "Total inches of rainfall   = " + format(totalInchesInRainFall) + "\n"    \
+         "Average rainfall per month = " + format(averageRainfall) + "\n"
+
+print(output)
