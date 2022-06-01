5b6cc555b47bbf9b4670fa8788241d9bafcbf288
jesushilarioh <jesushilariohernandez@gmail.com>
2022-06-01 18:47:21 -0500

    added and modified challenge 6 and 7

--- Starting Out with Python/Chapter 4/Programming Exercies/07.py
+++ Starting Out with Python/Chapter 4/Programming Exercies/07.py
@@ -7,9 +7,28 @@
 # period. The output should be displayed in a dollar amount, not the number of pennies.
 #
 #
-pennies = 0
+pennies = 0.01
+number_of_days = int(input("Enter the number of days: "))
+output = "Day\tPay\n" \
+         "----------------------\n"
+total_pay = 0.0
+day_pay = 0.0
 
+for day in range(number_of_days):
 
+    if (day == 0): 
+        day_pay = pennies
+        output += format(day + 1) + "\t$" + format(day_pay, '.2f') + "\n"
+        
+    else: 
+        day_pay = pennies
+        output += format(day + 1) + "\t$" + format(day_pay, '.2f') + "\n"
+        
+    total_pay += day_pay
+    pennies *= 2
+    
 
+output += "----------------------\n" \
+          "Total pay = $" + format(total_pay, '.2f') + '\n'
 
-
+print(output)
