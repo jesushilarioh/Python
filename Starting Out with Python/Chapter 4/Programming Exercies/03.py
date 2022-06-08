72a3b088918e8729b6b19cd4fe2fc9ff9161d277
jesushilarioh <jesushilariohernandez@gmail.com>
2022-06-08 15:39:08 -0500

    added

--- Starting Out with Python/Chapter 4/Programming Exercies/03.py
+++ Starting Out with Python/Chapter 4/Programming Exercies/03.py
@@ -19,11 +19,15 @@ totalLapTime = 0.0
 # of times that they have run around a racetrack
 numberAroundTrack = int(input("How many times around track? "))
 
+
 # 2. uses a loop to prompt them to enter the lap time for
 # each of their laps.
 for lapTime in range(numberAroundTrack):
 
     userLapTime = float(input("Enter lap time for lap #" + format(lapTime + 1) + " "))
+
+    if lapTime == 0:
+        slowestLapTime = fastestLapTime = userLapTime
 
     if userLapTime > fastestLapTime:
         fastestLapTime = userLapTime
@@ -35,11 +39,12 @@ for lapTime in range(numberAroundTrack):
 
 averageLapTime = totalLapTime / numberAroundTrack
 
+
 # 3.  When the loop finishes, the program 
 # should display the time of their fastest lap, the time of
 # their slowest lap, and their average lap time.
-output = "Fastest lap = " + format(fastestLapTime) + "\n"  \
-          "Slowest lap = " + format(slowestLapTime) + "\n"  \
-          "Average lap time = " + format(averageLapTime, '.2f') + ". "
+output = "Fastest lap      = " + format(fastestLapTime) + "\n"  \
+         "Slowest lap      = " + format(slowestLapTime) + "\n"  \
+         "Average lap time = " + format(averageLapTime) + ". "
 
 print(output)
